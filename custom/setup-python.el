9d1029f51867328a471172ce58e047b0198280f8
Dmitry Ulyanov <sillent1987@gmail.com>
2017-01-15 16:42:08 +0300

    change elpy-mode to jedi:setup in setup-python.el

--- custom/setup-python.el
+++ custom/setup-python.el
@@ -1,6 +1,8 @@
 ;;; setup-python --- Setup python
 ;;; Commentary:
 ;;; Code:
-(elpy-enable)
+;; (elpy-enable)
+(require 'jedi)
+(add-hook 'python-mode-hook 'jedi:setup)
 (provide 'setup-python)
 ;;; setup-python.el ends here
