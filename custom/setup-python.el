f90e9ed5640d516b4f0b323317fdfb9f80d4b2c7
Dmitry Ulyanov <sillent1987@gmail.com>
2017-02-01 01:02:04 +0300

    added indent-offset for python and elpy-rpc

--- custom/setup-python.el
+++ custom/setup-python.el
@@ -2,5 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 (elpy-enable)
+(setq python-indent-offset 4)
+(setq elpy-rpc-timeout 3)
+(setq elpy-rpc-backend "jedi")
 (provide 'setup-python)
 ;;; setup-python.el ends here
