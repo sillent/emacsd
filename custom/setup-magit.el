0def5512d0bb500d9ab71e8b33e7c7aaf9c55047
Dmitry Ulyanov <sillent1987@gmail.com>
2017-02-05 00:25:47 +0300

    added  keybinding for magit log, init, commit

--- custom/setup-magit.el
+++ custom/setup-magit.el
@@ -2,5 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 (global-set-key (kbd "C-x g s") 'magit-status)
+(global-set-key (kbd "C-x g l") 'magit-log)
+(global-set-key (kbd "C-x g i") 'magit-init)
+(global-set-key (kbd "C-x g c") 'magit-commit)
 (provide 'setup-magit)
 ;;; setup-magit ends here
