d87ad25c8cc58b9532074f95d9b41915f367d70a
Dmitry Ulyanov <sillent1987@gmail.com>
2017-06-09 22:32:11 +0300

    comment bad django-mode

--- custom/setup-python.el
+++ custom/setup-python.el
@@ -8,14 +8,14 @@
 (setq elpy-rpc-timeout 3)
 (setq elpy-rpc-backend "jedi")
 
-(add-hook 'python-mode-hook 'importmagic-mode)
+;; (add-hook 'python-mode-hook 'importmagic-mode)
 (require 'py-autopep8)
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 (setq py-autopep8-options '("--max-line-length=100"))
 
-(require 'django-html-mode)
-(require 'django-mode)
-(add-to-list 'auto-mode-alist '("\\.djhtml$" . django-html-mode))
+;; (require 'django-html-mode)
+;; (require 'django-mode)
+;; (add-to-list 'auto-mode-alist '("\\.djhtml$" . django-html-mode))
 
 (provide 'setup-python)
 ;;; setup-python.el ends here
