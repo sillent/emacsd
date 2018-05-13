2ab2b3765b3811feeb01b687736cb13df7022119
Dmitry Ulyanov <sillent1987@gmail.com>
2018-05-13 22:56:37 +0300

    deleted duplicate line

--- custom/setup-helm.el
+++ custom/setup-helm.el
@@ -23,7 +23,13 @@
      helm-ff-file-name-history-use-recentf t
      helm-move-to-line-cycle-in-source t
      helm-buffers-fuzzy-matching t
-     helm-display-header-line nil))
+     helm-display-header-line nil)
+    ;; hooks
+    (add-hook 'eshell-mode-hook
+              #'(lambda ()
+                  (define-key eshell-mode-map (kbd "M-l")  'helm-eshell-history)))
+    (add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
+    )
   :bind
   (
    ("M-x" . helm-M-x)
@@ -72,31 +78,9 @@
 (define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
 (define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
 
-(when (executable-find "curl")
-  (setq helm-google-suggest-use-curl-p t))
-
-(setq
- helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
- helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
- helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
- helm-candidate-number-limit 500 ; limit the number of displayed canidates
- helm-ff-file-name-history-use-recentf t
- helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
- helm-buffers-fuzzy-matching t          ; fuzzy matching buffer names when non-nil
-                                        ; useful in helm-mini that lists buffers
-
- )
 
 (add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
 
-;; (global-set-key (kbd "M-x") 'helm-M-x)
-;; (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-;; (global-set-key (kbd "C-x b") 'helm-mini)
-;; (global-set-key (kbd "C-x C-f") 'helm-find-files)
-;; (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
-;; (global-set-key (kbd "C-c h o") 'helm-occur)
-
-;; (global-set-key (kbd "C-c h C-c w") 'helm-wikipedia-suggest)
 
 ;; (global-set-key (kbd "C-c h x") 'helm-register)
 ;; (global-set-key (kbd "C-x r j") 'jump-to-register)
