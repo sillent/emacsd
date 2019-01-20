c92892fcc0f75905c527f4421ad6edfe5c32d3ea
Dmitry Ulyanov <dmitry.ulyanov@stoloto.ru>
2019-01-20 12:09:27 +0300

    my setup-rust implementation
    Signed-off-by: Dmitry Ulyanov <dmitry.ulyanov@stoloto.ru>

--- custom/setup-rust.el
+++ custom/setup-rust.el
@@ -5,5 +5,19 @@
 ;;; Email: siilent1987@yahoo.com
 ;;; Code:
 
+(use-package rust-mode
+  :ensure t
+  :config
+  (progn
+    (autoload 'rust-mode "rust-mode" nil t)
+    (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+    ))
+(use-package racer
+  :ensure t
+  :config
+  (progn
+    (add-hook 'rust-mode-hook #'racer-mode)
+    (add-hook 'racer-mode-hook #'eldoc-mode)
+    (add-hook 'racer-mode-hook #'company-mode)))
 (provide 'setup-rust)
 ;;; ends - setup-rust.el
