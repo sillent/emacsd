44844befcf7915074ec2c585655769cc66bfb077
Dmitry Ulyanov <dmitry.ulyanov@stoloto.ru>
2019-01-20 11:02:04 +0300

    fixing error use-rust

---
+++ custom/setup-rust.el
@@ -0,0 +1,9 @@
+;;; setup-rust.el --- Init Emacs configuration
+;;; Commentary:
+;;; This is my emacs.d/init.el configuration
+;;; Author: Dmitry Ulyanov
+;;; Email: siilent1987@yahoo.com
+;;; Code:
+
+(provide 'setup-rust)
+;;; ends - setup-rust.el
