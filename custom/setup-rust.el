a0bb183f145eb0f275ea17ddf5fe81fbaabab5d4
Dmitry Ulyanov <dmitry.ulyanov@stoloto.ru>
2019-01-20 16:07:40 +0300

    setup rust added
    Signed-off-by: Dmitry Ulyanov <dmitry.ulyanov@stoloto.ru>

--- custom/setup-rust.el
+++ custom/setup-rust.el
@@ -4,20 +4,48 @@
 ;;; Author: Dmitry Ulyanov
 ;;; Email: siilent1987@yahoo.com
 ;;; Code:
+(use-package flycheck-rust
+  :ensure t
+  :defer t
+  :config
+  (add-hook  'flycheck-mode-hook #'flycheck-rust-setup))
+
+(use-package company-racer
+  :ensure t
+  :defer t
+  )
 
 (use-package rust-mode
   :ensure t
+  :defer t
+  :init
+  (require 'rust-mode)
+  (global-company-mode)
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
   :config
-  (progn
-    (autoload 'rust-mode "rust-mode" nil t)
-    (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-    ))
-(use-package racer
-  :ensure t
-  :config
-  (progn
-    (add-hook 'rust-mode-hook #'racer-mode)
+  (use-package company-racer)
+  (use-package flycheck-rust)
+  (use-package racer
+    :ensure t
+    :defer t
+    :init
+    (setq racer-rust-src-path (concat (getenv "HOME")
+                                      "/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"))
+    (setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
+    :config
+    (define-key rust-mode-map (kbd "M-\"") #'racer-find-definition)
     (add-hook 'racer-mode-hook #'eldoc-mode)
-    (add-hook 'racer-mode-hook #'company-mode)))
+    (add-hook 'racer-mode-hook #'company-mode)
+    (local-set-key (kbd "TAB") #'company-indent-or-complete-common)
+    (setq company-tooltip-align-annotations t)
+    )
+  (defun my-rust-mode-hook()
+    (set (make-local-variable 'compile-command "~/.cargo/bin/cargo run"))
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+    )
+  (add-hook 'rust-mode-hook 'my-rust-mode-hook)
+  (add-hook 'rust-mode-hook #'racer-mode)
+  )
 (provide 'setup-rust)
-;;; ends - setup-rust.el
+
+;;; setup-rust.el ends here
