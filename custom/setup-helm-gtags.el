7eedfd0a5166e6ac4452b7d9b1fd7ffcd405ec33
Dmitry Ulyanov <dmitry.ulyanov@stoloto.ru>
2019-04-11 23:25:57 +0300

    fixed helm-gtags require

--- custom/setup-helm-gtags.el
+++ custom/setup-helm-gtags.el
@@ -2,37 +2,40 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'helm-gtags)
-
-(setq
- helm-gtags-ignore-case t               ; ignore case in seearch
- helm-gtags-auto-update t               ; auto update file TAG on save
- helm-gtags-use-input-at-cursor t
- helm-gtags-pulse-at-cursor t
- helm-gtags-prefix-key "\C-cg"
- helm-gtags-suggested-key-mapping t
- )
+(use-package helm-gtags
+  :ensure t
+  :config
+  (progn
+    (setq
+     helm-gtags-ignore-case t               ; ignore case in seearch
+     helm-gtags-auto-update t               ; auto update file TAG on save
+     helm-gtags-use-input-at-cursor t
+     helm-gtags-pulse-at-cursor t
+     helm-gtags-prefix-key "\C-cg"
+     helm-gtags-suggested-key-mapping t
+     )
 
 ;; Enable helm-gtags-mode in Dired so you can jump to any tag
 ;; when navigate project tree with Dired
-(add-hook 'dired-mode-hook 'helm-gtags-mode)
+    (add-hook 'dired-mode-hook 'helm-gtags-mode)
 
 ;; Enable helm-gtags-mode in Eshell for the same reason as above
-(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+    (add-hook 'eshell-mode-hook 'helm-gtags-mode)
 
 ;; Enable helm-gtags-mode in languages that GNU Global supports
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-(add-hook 'java-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook 'helm-gtags-mode)
+    (add-hook 'c-mode-hook 'helm-gtags-mode)
+    (add-hook 'c++-mode-hook 'helm-gtags-mode)
+    (add-hook 'java-mode-hook 'helm-gtags-mode)
+    (add-hook 'asm-mode-hook 'helm-gtags-mode)
 
 ;; key bindings
-(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+    (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+    (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+    (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+    (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+    (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+    (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+    ))
 
 (provide 'setup-helm-gtags)
 ;;; setup-helm-gtags.el ends here
