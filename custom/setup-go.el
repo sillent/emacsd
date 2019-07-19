85b2a8205fa14c6c3c7d0d0014dcd3006fb9f715
Dmitry Ulyanov <dmitry.ulyanov@stoloto.ru>
2019-07-19 09:35:40 +0300

    go setup file
    Signed-off-by: Dmitry Ulyanov <dmitry.ulyanov@stoloto.ru>

--- custom/setup-go.el
+++ custom/setup-go.el
@@ -11,6 +11,16 @@
   :ensure t)
 (use-package company-go
   :ensure t)
+(use-package go-rename
+  :ensure t)
+(use-package gotest
+  :ensure t)
+(use-package go-scratch
+  :ensure t)
+(use-package go-direx
+  :ensure t)
+(use-package go-guru
+  :ensure t)
 (use-package go-mode
   :ensure t
   :config
@@ -19,6 +29,15 @@
     (add-hook 'go-mode-hook (lambda ()
                               (set (make-local-variable 'company-backends) '(company-go))
                               (company-mode)
-                              ))))
+                              ))
+    (add-hook 'go-mode-hook 'yas-minor-mode)
+    (add-hook 'go-mode-hook 'flycheck-mode)
+    (setq multi-compile-alist '(
+        (go-mode . (
+        ("go-build" "go build -v"
+           (locate-dominating-file buffer-file-name ".git"))
+           ("go-build-and-run" "go build -v && echo 'build finish' && eval ./${PWD##*/}"
+              (multi-compile-locate-file-dir ".git"))))
+                  ))))
 (provide 'setup-go)
 ;;; setup-go.el ends here
